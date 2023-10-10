ae3738a88d2d8f076c07b4346a200fe2874b7f3f
Eno <k1990121@gmail.com>
2023-10-11 02:49:17 +0900

    추가
    - 환경변수 치환전에 사용할 환경변수용 py
    - 로그용 파일 만들었으나 동작하지 않음 추후에 수정해야함
    - 잡다한 유틸용도로 쓸 py

---
+++ util/my_env.py
@@ -0,0 +1,3 @@
+""" env 변수 정의 파일 """
+BACKEND_URL = "teemo-world.link"
+BACKEND_PORT = "3000"
---
+++ util/my_util.py
@@ -0,0 +1,14 @@
+""" 잡다한 함수로 뺼 것들 모아놓은곳"""
+import requests
+
+from util.my_env import BACKEND_PORT, BACKEND_URL
+
+
+async def getCommands(panda_id: str):
+    """panda_id의 command리스트를 가져온다"""
+    commands = requests.get(
+        url=f"http://{BACKEND_URL}:{BACKEND_PORT}/user/command/{panda_id}",
+        timeout=5,
+    )
+    command_dict = commands.json()
+    return command_dict
