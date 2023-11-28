2b9ca10dcc664376febf1432dfd3e4c669d88baf
Eno <k1990121@gmail.com>
2023-11-28 10:50:35 +0900

    manager check 실패시 더미 아이디로 로그인하게해서 recatpcha안걸리게 변경

--- watch.py
+++ watch.py
@@ -67,81 +67,90 @@ async def test():
 @app.get("/check-manager", status_code=status.HTTP_200_OK)
 async def check_manager_login(manager_id: str, manager_pw: str, response: Response):
     """매니저로 사용하는 id/pw가 로그인이 가능한지 확인하는 함수"""
+    print(manager_id)
+    print(manager_pw)
+    apw = await async_playwright().start()
+    browser = await apw.chromium.launch(headless=HEADLESS)
+    page = await browser.new_page()
     try:
-      print(manager_id)
-      print(manager_pw)
-      apw = await async_playwright().start()
-      browser = await apw.chromium.launch(headless=HEADLESS)
-      page = await browser.new_page()
-      await page.goto("http://pandalive.co.kr")
-      await page.get_by_role("button", name="닫기").click()
-      await page.get_by_role("button", name="로그인 / 회원가입").click()
-      await page.get_by_role("link", name="로그인 / 회원가입").click()
-      await page.get_by_role("link", name="로그인").click()
-      # await page.get_by_role("textbox").nth(1).fill("siveriness0")
-      # await page.get_by_role("textbox").nth(2).fill("dkflfkd12#")
-      await page.get_by_role("textbox").nth(1).fill(manager_id)
-      await page.get_by_role("textbox").nth(2).fill(manager_pw)
-      await asyncio.sleep(2)
-      await page.get_by_role("button", name="로그인", exact=True).click()
-      await asyncio.sleep(2)
-      invalid_text_id = await page.get_by_text("존재하지 않는 사용자입니다.").is_visible()
-      invalid_text_pw = await page.get_by_text("비밀번호가 일치하지 않습니다.다시 입력해 주세요.").is_visible()
-      if invalid_text_id or invalid_text_pw:
-          await browser.close()
-          raise ex.PlayWrightException("Invalid Id/PW")  # pylint: disable=W0719
-      invalid_label_id = await page.get_by_label("존재하지 않는 사용자입니다.").is_visible()
-      invalid_label_pw = await page.get_by_label(
-          "비밀번호가 일치하지 않습니다.다시 입력해 주세요."
-      ).is_visible()
-      if invalid_label_id or invalid_label_pw:
-          await browser.close()
-          raise ex.PlayWrightException("Invalid Id/PW")  # pylint: disable=W0719
-      invalid_login_detect = await page.get_by_label(
-          "비정상적인 로그인이 감지되었습니다.잠시 후 다시 시도해 주세요."
-      ).is_visible()
-      auto_detect = await page.get_by_label("자동접속방지 체크박스를 확인해주세요").is_visible()
-      print(invalid_login_detect, auto_detect)
-      if invalid_login_detect or auto_detect:
-          # print("Invliad login popup")
-          await page.get_by_role("button", name="확인").click()
-          await asyncio.sleep(2)
-          click_frame = None
-          show_frame = None
-          frames = page.frames
-          for frame in frames:
-              print(frame.name)
-              if "/api2/bframe" in frame.url:
-                  show_frame = page.frame_locator(f'iframe[name="{frame.name}"]')
-              if "/api2/anchor" in frame.url:
-                  click_frame = page.frame_locator(f'iframe[name="{frame.name}"]')
-          await click_frame.get_by_label("로봇이 아닙니다.").click()
-          await show_frame.get_by_role("button", name="음성 보안문자 듣기").click()
-          audio_url = await show_frame.get_by_role(
-              "link", name="또는 오디오를 MP3로 다운로드하세요."
-          ).get_attribute("href")
-          print(audio_url)
-          os.system(f"curl {audio_url} --output stt/audio.mp3")
-          response = sample_recognize("stt/audio.mp3")
-          if response:
-              print(response)
-              await show_frame.get_by_label("들리는 대로 입력하세요.").fill(response)
-              await show_frame.get_by_role("button", name="확인").click()
-              await page.get_by_role("button", name="로그인", exact=True).click()
-              await page.wait_for_selector("div.profile_img")
-          else:
-              print("stt 실패")
-              await browser.close()
-              raise ex.PlayWrightException("stt 실패")  # pylint: disable=W0719
-      else:
-          print("로그인 성공")
-          login_profile = await page.query_selector("div.profile_img")
-          manager_nickname = await login_profile.inner_text()
-          await browser.close()
-          return manager_nickname
-    except: 
-      await page.screenshot(path="error.png")
-      return "failed"
+        await page.goto("http://pandalive.co.kr")
+        await page.get_by_role("button", name="닫기").click()
+        await page.get_by_role("button", name="로그인 / 회원가입").click()
+        await page.get_by_role("link", name="로그인 / 회원가입").click()
+        await page.get_by_role("link", name="로그인").click()
+        # await page.get_by_role("textbox").nth(1).fill("siveriness0")
+        # await page.get_by_role("textbox").nth(2).fill("dkflfkd12#")
+        await page.get_by_role("textbox").nth(1).fill(manager_id)
+        await page.get_by_role("textbox").nth(2).fill(manager_pw)
+        await asyncio.sleep(2)
+        await page.get_by_role("button", name="로그인", exact=True).click()
+        await asyncio.sleep(2)
+        invalid_text_id = await page.get_by_text("존재하지 않는 사용자입니다.").is_visible()
+        invalid_text_pw = await page.get_by_text("비밀번호가 일치하지 않습니다.다시 입력해 주세요.").is_visible()
+        if invalid_text_id or invalid_text_pw:
+            await browser.close()
+            raise ex.PlayWrightException("Invalid Id/PW")  # pylint: disable=W0719
+        invalid_label_id = await page.get_by_label("존재하지 않는 사용자입니다.").is_visible()
+        invalid_label_pw = await page.get_by_label(
+            "비밀번호가 일치하지 않습니다.다시 입력해 주세요."
+        ).is_visible()
+        if invalid_label_id or invalid_label_pw:
+            await browser.close()
+            raise ex.PlayWrightException("Invalid Id/PW")  # pylint: disable=W0719
+        invalid_login_detect = await page.get_by_label(
+            "비정상적인 로그인이 감지되었습니다.잠시 후 다시 시도해 주세요."
+        ).is_visible()
+        auto_detect = await page.get_by_label("자동접속방지 체크박스를 확인해주세요").is_visible()
+        print(invalid_login_detect, auto_detect)
+        if invalid_login_detect or auto_detect:
+            # print("Invliad login popup")
+            await page.get_by_role("button", name="확인").click()
+            await asyncio.sleep(2)
+            click_frame = None
+            show_frame = None
+            frames = page.frames
+            for frame in frames:
+                print(frame.name)
+                if "/api2/bframe" in frame.url:
+                    show_frame = page.frame_locator(f'iframe[name="{frame.name}"]')
+                if "/api2/anchor" in frame.url:
+                    click_frame = page.frame_locator(f'iframe[name="{frame.name}"]')
+            await click_frame.get_by_label("로봇이 아닙니다.").click()
+            await show_frame.get_by_role("button", name="음성 보안문자 듣기").click()
+            audio_url = await show_frame.get_by_role(
+                "link", name="또는 오디오를 MP3로 다운로드하세요."
+            ).get_attribute("href")
+            print(audio_url)
+            os.system(f"curl {audio_url} --output stt/audio.mp3")
+            response = sample_recognize("stt/audio.mp3")
+            if response:
+                print(response)
+                await show_frame.get_by_label("들리는 대로 입력하세요.").fill(response)
+                await show_frame.get_by_role("button", name="확인").click()
+                await page.get_by_role("button", name="로그인", exact=True).click()
+                await page.wait_for_selector("div.profile_img")
+            else:
+                print("stt 실패")
+                await browser.close()
+                raise ex.PlayWrightException("stt 실패")  # pylint: disable=W0719
+        else:
+            print("로그인 성공")
+            login_profile = await page.query_selector("div.profile_img")
+            manager_nickname = await login_profile.inner_text()
+            await browser.close()
+            return manager_nickname
+    except: # pylint: disable=W0718 W0702
+        await page.goto("http://pandalive.co.kr")
+        await page.get_by_role("button", name="닫기").click()
+        await page.get_by_role("button", name="로그인 / 회원가입").click()
+        await page.get_by_role("link", name="로그인 / 회원가입").click()
+        await page.get_by_role("link", name="로그인").click()
+        await page.get_by_role("textbox").nth(1).fill("resetaccount")
+        await page.get_by_role("textbox").nth(2).fill("Adkflfkd1")
+        await asyncio.sleep(2)
+        await page.get_by_role("button", name="로그인", exact=True).click()
+        await asyncio.sleep(2)
+        raise ex.PlayWrightException("Invalid Id/PW")  # pylint: disable=W0719 W0707
   
 
 
