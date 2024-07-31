8eae29955459ee25c8b0e285f2c850d867ba9295
eno <k1990121@gmail.com>
2024-07-31 13:25:08 +0900

    middle save

--- watch.py
+++ watch.py
@@ -17,7 +17,6 @@ from playwright.async_api import FrameLocator
 from dotenv import load_dotenv
 from classes import playwright_watch as pws
 from custom_exception import custom_exceptions as ex
-from stt_v2 import sample_recognize
 from util.my_util import logging_debug, logging_error, logging_info
 
 load_dotenv()
@@ -54,8 +53,8 @@ async def loop2():
 async def night_watch_start():
     """감시자 시작"""
     await play_watch.login()
-    await asyncio.sleep(1)
-    asyncio.create_task(loop2())
+    # await asyncio.sleep(1)
+    # asyncio.create_task(loop2())
     return {"message": "NightWatch"}
 
 
