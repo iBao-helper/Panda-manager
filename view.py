1e3f8d4e7e870ed4bb7924d21dd5be0a019c7aab
eno <k1990121@gmail.com>
2024-11-02 04:49:20 +0900

    a

--- view.py
+++ view.py
@@ -452,7 +452,10 @@ async def disconnect_proxy_by_ip(user_id: str):
     copy_dict = app.ws_dict.copy()
     for key, value in copy_dict.items():
         if value.user_id == user_id:
-            app.thread_lists.remove(value.random_string)
+            try:
+                app.thread_lists.remove(value.random_string)
+            except:
+                pass
     print(app.ws_dict.keys())
     print(app.thread_lists)
     return JSONResponse(
