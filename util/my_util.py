9384cfca97aede75f32f7bfde8b964eab157a648
eno <k1990121@gmail.com>
2024-11-04 20:16:09 +0900

    a

--- util/my_util.py
+++ util/my_util.py
@@ -153,10 +153,11 @@ async def send_hart_history(user_id: str, nickname: str, bj_name, hart_count):
     """하트 내역 전송"""
     try:
         requests.post(
-            url=f"http://{BACKEND_URL}:{BACKEND_PORT}/bj/hart-history/{bj_name}",
+            url=f"http://{BACKEND_URL}:{BACKEND_PORT}/bj/hart-history/tracker",
             json={
                 "nickname": emoji.emojize(nickname),
                 "user_id": emoji.emojize(user_id),
+                "bj_name=": emoji.emojize(bj_name),
                 "count": hart_count,
             },
             timeout=5,
